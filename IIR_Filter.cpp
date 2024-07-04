5575216cb6a4ada5e0856d14f1f39b4f59e3f988
ku-rihara <k023g0027@g.neec.ac.jp>
2024-07-04 18:18:03 +0900

    a

--- IIR_Filter.cpp
+++ IIR_Filter.cpp
@@ -2,22 +2,24 @@
 #include"wave.h"
 
 void IIR_LPF(const double& fc, const double& Q, std::vector<double>& a, std::vector<double>& b) {
+
 	const double cutoffFrequency = tan(M_PI * fc) / (2.0 * M_PI);//アナログ遮断周波数
-	a[0] = 1.0 + 2.0 * M_PI * cutoffFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2);
-	a[1] = (8.0 * std::pow(M_PI, 2) * std::pow(fc, 2) - 2.0) / a[0];
-	a[2] = (1.0 - 2.0 * M_PI * fc / Q + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / a[0];
-	b[0] = 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2) / a[0];
-	b[1] = 8.0 * std::pow(M_PI, 2) * std::pow(fc, 2) / a[0];
-	b[2] = 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2) / a[0];
+
+	a[0] = 1.0 + 2.0 * M_PI * cutoffFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2);
+	a[1] = (8.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2) - 2.0) / a[0];
+	a[2] = (1.0 - 2.0 * M_PI * cutoffFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2)) / a[0];
+	b[0] = (4.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2)) / a[0];
+	b[1] = (8.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2)) / a[0];
+	b[2] = (4.0 * std::pow(M_PI, 2) * std::pow(cutoffFrequency, 2)) / a[0];
 
 	a[0] = 1.0;
 }
 
 void IIR_HPF(const double& fc, const double& Q, std::vector<double>& a, std::vector<double>& b) {
 	const double analogFrequency = tan(M_PI * fc) / (2.0 * M_PI);//アナログ遮断周波数
-	a[0] = 1.0 + 2.0 * M_PI * analogFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2);
-	a[1] = (8.0 * std::pow(M_PI, 2) * std::pow(fc, 2) - 2.0) / a[0];
-	a[2] = (1.0 - 2.0 * M_PI * fc / Q + 4.0 * std::pow(M_PI, 2) * std::pow(fc, 2)) / a[0];
+	a[0] = 1.0 + 2.0 * M_PI * analogFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(analogFrequency, 2);
+	a[1] = (8.0 * std::pow(M_PI, 2) * std::pow(analogFrequency, 2) - 2.0) / a[0];
+	a[2] = (1.0 - 2.0 * M_PI * analogFrequency / Q + 4.0 * std::pow(M_PI, 2) * std::pow(analogFrequency, 2)) / a[0];
 	b[0] = 1.0 / a[0];
 	b[1] = -2 / a[0];
 	b[2] = 1.0 / a[0];
@@ -25,7 +27,7 @@ void IIR_HPF(const double& fc, const double& Q, std::vector<double>& a, std::vec
 	a[0] = 1.0;
 }
 
-void IIR_BPF(const double& fcLow, const double& fcHight,std::vector<double>& a, std::vector<double>& b) {
+void IIR_BPF(const double& fcLow, const double& fcHight, std::vector<double>& a, std::vector<double>& b) {
 	const double analogFrequencyLow = tan(M_PI * fcLow) / (2.0 * M_PI);//アナログ遮断周波数(低い)
 	const double analogFrequencyHight = tan(M_PI * fcHight) / (2.0 * M_PI);//アナログ遮断周波数(高い)
 
@@ -36,7 +38,7 @@ void IIR_BPF(const double& fcLow, const double& fcHight,std::vector<double>& a, 
 	b[1] = 0.0;
 	b[2] = (-2.0 * M_PI * (analogFrequencyHight - analogFrequencyLow)) / a[0];
 
-	a[0]=1.0;
+	a[0] = 1.0;
 }
 
 
